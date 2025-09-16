511dbce2e57e146beec7152a12b6cd4cbc6c2bf1
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-09-16 20:15:26 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyADteWmWhm-6bMSRBA76JBSV32DpLCUels",
+  authDomain: "controle-garantia.firebaseapp.com",
+  projectId: "controle-garantia",
+  storageBucket: "controle-garantia.appspot.com",
+  messagingSenderId: "753625861980",
+  appId: "1:753625861980:web:4fba368d822bff7c77fb99",
+  measurementId: "G-ZHP4ZMLKLV"
 };
 
 // Initialize Firebase
