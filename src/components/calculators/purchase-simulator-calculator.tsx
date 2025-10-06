b59a890ad2429e2923572677182d1c65f947eff4
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-10-06 18:03:51 +0000

    npm run lint
    > synergia-os@0.1.0 lint
    > next lint
    
    ./src/components/ca

--- src/components/calculators/purchase-simulator-calculator.tsx
+++ src/components/calculators/purchase-simulator-calculator.tsx
@@ -14,7 +14,7 @@ import { useToast } from "@/hooks/use-toast";
 import { formatCurrency, formatNumber } from "@/lib/utils";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "../ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
-import type { NfeInfo, PurchaseSimulation } from "@/lib/types";
+import type { NfeInfo, PurchaseSimulation, SimulatedItemData } from "@/lib/types";
 import * as db from '@/lib/db';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "../ui/alert-dialog";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, DialogClose, DialogDescription } from "@/components/ui/dialog";
