0ea9bb0501a07d8230750982c9b77f90ca0384f5
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-10-09 17:20:40 +0000

    tem esse ainda, que so de ver o nome DO ARQUIV JA ME DA MEDO, MIDDLEWARE

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(_request: NextRequest) {
+export async function middleware() {
   // A lógica de autenticação foi temporariamente desativada para permitir o desenvolvimento.
   // Para reativar, descomente o bloco abaixo.
   /*
