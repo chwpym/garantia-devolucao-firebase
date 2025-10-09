eef8ed6c0299d5dce401a8a87e34030ff8f260f6
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-10-09 14:32:44 +0000

    Perfeito! Essa nova informação é a mais clara de todas e nos dá a respos

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-import { getAuth, GoogleAuthProvider, connectAuthEmulator } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,15 +18,5 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const storage = getStorage(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-
-// Conecta ao emulador em ambiente de desenvolvimento
-if (process.env.NODE_ENV === 'development') {
-  try {
-    connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true } );
-    console.log("🔥 Conectado ao Emulador de Autenticação do Firebase");
-  } catch (error) {
-    console.error("Falha ao conectar ao emulador de auth", error);
-  }
-}
 
 export { app, storage, auth, googleProvider };
