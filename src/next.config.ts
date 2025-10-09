050c6d957fa7d308d9a5705dfbfe98b67b6cb7c8
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-10-09 17:38:30 +0000

    O NEXT-PWA NÃO PODE SER REMOVIDO, EU USO O APP NO WINDOWS COMO UM PWA, M

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true, // Garante que o novo Service Worker ative imediatamente
+  // runtimeCaching, // Descomente para configurar estratégias de cache
+  disable: process.env.NODE_ENV === 'development',
+});
+
+// A forma correta de exportar a configuração é simplesmente passando o nextConfig para a função do PWA.
+// A manipulação de .env é feita automaticamente pelo Next.js.
+export default pwaConfig(nextConfig);
